991bfc2334dcd2d87f5f8dd37083ad309b4982e2
Panchakshari Matada <panchaksharimatada@gmail.com>
2026-06-02 13:43:05 +0530

    Add files via upload

---
+++ class assginments/13th- feb-task/oddoreven.js
@@ -0,0 +1,9 @@
+let number = 5;
+if(number % 2 ==0) 
+{
+    console.log("Even");
+}
+else 
+{
+    console.log("Odd");
+}
---
+++ class assginments/13th- feb-task/script.js
@@ -0,0 +1,6 @@
+let name = 'nagaraj';
+let age = 22;
+let favt_color = 'green';
+console.log(name);
+console.log(age);
+console.log(favt_color);
