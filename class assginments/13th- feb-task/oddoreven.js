f0feccbc894d56efee9d888e8f38f622ef1daf67
Panchakshari Matada <panchaksharimatada@gmail.com>
2026-06-02 13:44:48 +0530

    Delete class assginments directory

--- class assginments/13th- feb-task/oddoreven.js
+++
@@ -1,9 +0,0 @@
-let number = 5;
-if(number % 2 ==0) 
-{
-    console.log("Even");
-}
-else 
-{
-    console.log("Odd");
-}
--- class assginments/13th- feb-task/script.js
+++
@@ -1,6 +0,0 @@
-let name = 'nagaraj';
-let age = 22;
-let favt_color = 'green';
-console.log(name);
-console.log(age);
-console.log(favt_color);
